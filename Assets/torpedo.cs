2c63807ca610bc6d07d6c553d1e49e4a73797bb9
Jose <jbergon@iesflorenciopintado.es>
2021-01-14 11:07:56 +0100

    primer commit

---
+++ Assets/torpedo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torpedo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int contador = 0;
+
+    void Start()
+    {
+
+        Debug.Log("Hola mundo");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.1f,0);
+        
+    }
+}
