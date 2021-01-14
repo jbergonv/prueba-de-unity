371c7a4a1a7c127e4d96a2a39130acb383fd98ac
Jose <jbergon@iesflorenciopintado.es>
2021-01-14 11:09:27 +0100

    primer commit

--- Assets/torpedo.cs
+++ Assets/torpedo.cs
@@ -18,7 +18,7 @@ public class torpedo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, -0.1f,0);
+        transform.Translate(0, -0.5f,0);
         
     }
 }
